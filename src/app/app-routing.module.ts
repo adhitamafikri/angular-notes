46df3272414b65cbf56d81635746c9c1441543db
AdhitamaFikri <adhitamafikri@gmail.com>
2020-06-15 23:17:28 +0700

    chore: remove old directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomePageModule } from './pages/home-page/home-page.module';
 import { CreateNotePageComponent } from './pages/create-note-page/create-note-page.component';
+import { CreateNotePageModule } from './pages/create-note-page/create-note-page.module';
 
 const routes: Routes = [
-  // { path: '', component: HomePageComponent },
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/home-page/home-page.module').then(
-        (m) => m.HomePageModule
-      ),
-  },
+  { path: '', component: HomePageComponent },
   { path: 'create', component: CreateNotePageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomePageModule, CreateNotePageModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
